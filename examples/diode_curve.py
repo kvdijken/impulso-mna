8ce94c53bc7ba169e48ac37ffbeb7e8e32b73706
kvdijken <koen.van.dijken@gmail.com>
2026-06-12 21:46:36 +0200

    simpler implementation

--- examples/diode_curve.py
+++ examples/diode_curve.py
@@ -5,23 +5,22 @@ from impulso import *
 
 plt.rcParams['axes.xmargin'] = 0
 
-@np.vectorize
-def current(v):
-    global first
-    V1 = DCVoltageSource(voltage=v, id='V1')
-    R1 = Resistor(resistance=0.568, id='R1')
-    D1 = Diode(id='D1')
-    D1.DIODE_LIMITER_NVT = 10
-    circuit = Circuit()
-    circuit.add(V1, [0, 1])
-    circuit.add(R1, [1, 2])
-    circuit.add(D1, [2, 0])
-    v, i = solve_dc(circuit)
-    return v[2], i[D1]
+V1 = DCVoltageSource(voltage=1, id='V1')
+R1 = Resistor(resistance=0.568, id='R1')
+D1 = Diode(id='D1')
+D1.DIODE_LIMITER_NVT = 10
+circuit = Circuit()
+circuit.add(V1, [0, 1])
+circuit.add(R1, [1, 2])
+circuit.add(D1, [2, 0])
+
+print(circuit)
 
 v_ = np.linspace(-.1, 1, 100)
-data = current(v_)
-vd_, id_ = data[0], data[1]
+v, i = dc_sweep(circuit, V1, v_, show_output=True)
+
+vd_ = magify(v_pivot(v)[2])
+id_ = magify(i_pivot(i)[D1])
 
 plt.plot(np.abs(vd_),np.abs(id_),'k')
 plt.xlabel('Vd (V)')
@@ -29,5 +28,3 @@ plt.ylabel('Id (A)')
 plt.title('Diode I-V Curve')
 plt.grid()
 plt.show()
-
-
